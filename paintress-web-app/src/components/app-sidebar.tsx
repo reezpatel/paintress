552a0711b867f9c325550a41a0209ee384c2c31c
Reez <reezpatel@gmail.com>
2025-07-31 12:12:44 +0530

    remove unsed imports

--- paintress-web-app/src/components/app-sidebar.tsx
+++ paintress-web-app/src/components/app-sidebar.tsx
@@ -494,40 +494,40 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   );
 }
 
-function Tree({ item }: { item: string | any[] }) {
-  const [name, ...items] = Array.isArray(item) ? item : [item];
-  if (!items.length) {
-    return (
-      <SidebarMenuButton
-        isActive={name === "button.tsx"}
-        className="data-[active=true]:bg-transparent"
-      >
-        <File />
-        {name}
-      </SidebarMenuButton>
-    );
-  }
-  return (
-    <SidebarMenuItem>
-      <Collapsible
-        className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
-        defaultOpen={name === "components" || name === "ui"}
-      >
-        <CollapsibleTrigger asChild>
-          <SidebarMenuButton>
-            <ChevronRight className="transition-transform" />
-            <Folder />
-            {name}
-          </SidebarMenuButton>
-        </CollapsibleTrigger>
-        <CollapsibleContent>
-          <SidebarMenuSub>
-            {items.map((subItem, index) => (
-              <Tree key={index} item={subItem} />
-            ))}
-          </SidebarMenuSub>
-        </CollapsibleContent>
-      </Collapsible>
-    </SidebarMenuItem>
-  );
-}
+// function Tree({ item }: { item: string | any[] }) {
+//   const [name, ...items] = Array.isArray(item) ? item : [item];
+//   if (!items.length) {
+//     return (
+//       <SidebarMenuButton
+//         isActive={name === "button.tsx"}
+//         className="data-[active=true]:bg-transparent"
+//       >
+//         <File />
+//         {name}
+//       </SidebarMenuButton>
+//     );
+//   }
+//   return (
+//     <SidebarMenuItem>
+//       <Collapsible
+//         className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+//         defaultOpen={name === "components" || name === "ui"}
+//       >
+//         <CollapsibleTrigger asChild>
+//           <SidebarMenuButton>
+//             <ChevronRight className="transition-transform" />
+//             <Folder />
+//             {name}
+//           </SidebarMenuButton>
+//         </CollapsibleTrigger>
+//         <CollapsibleContent>
+//           <SidebarMenuSub>
+//             {items.map((subItem, index) => (
+//               <Tree key={index} item={subItem} />
+//             ))}
+//           </SidebarMenuSub>
+//         </CollapsibleContent>
+//       </Collapsible>
+//     </SidebarMenuItem>
+//   );
+// }
