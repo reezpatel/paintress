26fbfcb58f915d29295e2ab3343c58a9785945ce
Reez <reezpatel@gmail.com>
2025-07-31 12:35:21 +0530

    fix schema issue

--- paintress-server/app/database.py
+++ paintress-server/app/database.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
-from sqlalchemy.schema import CreateSchema
 from .config import settings
 
 
